328c4fe4a0a656e09bf505f6942f5db7abcf8537
collazoa7181 <collazoa7181@student.faytechcc.edu>
2025-09-19 12:25:41 -0400

    Add files via upload

---
+++ P1LAB1_CollazoAngel.py
@@ -0,0 +1,12 @@
+#Angel Collazo Santiago
+#September 19, 2025
+#P1LAB1
+#This project is about making a program where the user has to input their name and last name in separate and as result the user will recived a welcome messege back.
+first_name= input("Enter your first name: ")
+
+last_name= input("Enter your last name: ")
+
+print("Welcome to CTI-110", first_name, last_name +"!")
+
+
+
